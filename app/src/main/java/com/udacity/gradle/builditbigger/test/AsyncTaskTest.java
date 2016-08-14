a7d0ccc305638a412a3d2e07c9699df422974dee
Akshit Gupta <akshitonline@gmail.com>
2016-08-14 22:06:13 +0530

    Added functional tests

---
+++ app/src/main/java/com/udacity/gradle/builditbigger/test/AsyncTaskTest.java
@@ -0,0 +1,35 @@
+package com.udacity.gradle.builditbigger.test;
+
+import android.content.Context;
+import android.test.AndroidTestCase;
+import android.util.Pair;
+
+import com.udacity.gradle.builditbigger.EndpointsAsyncTask;
+
+import org.junit.Test;
+
+import java.util.concurrent.ExecutionException;
+
+/**
+ * Created by akshitgupta on 14/08/16.
+ */
+public class AsyncTaskTest extends AndroidTestCase {
+
+    @Test
+    public void verifyAsyncTaskResponse() {
+
+        EndpointsAsyncTask task = new EndpointsAsyncTask();
+        task.execute(new Pair<Context, String>(getContext(), "Manfred"));
+        String text = null;
+        try {
+            text = task.get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+
+        assertFalse("Async Task is producing empty string", text.length() == 0);
+    }
+}
